2dc311dbc6d7e093201201d5fed9c89528ced3d4
moshiurbd1 <moshiurbd101@gmail.com>
2025-05-08 12:27:49 +0600

    Basic Calculator with class

--- Class.php
+++ Class.php
@@ -1,4 +1,33 @@
 <?php
-echo "hello class";
+class Calculator{
+    public $x;
+    public $y;
+
+    public function Setter($a,$b){
+        $this->x=$a;
+        $this->y=$b;
+    }
+    public function sum(){
+        return $this->x+$this->y;
+    }
+    public function sub(){
+        return $this->y-$this->x;
+    }
+    public function mul(){
+        return $this->x*$this->y;
+    }
+    public function div(){
+        return $this->y/$this->x;
+    }
+
+}
+
+$obj=new Calculator();
+$obj->Setter(10,20);
+echo $obj->x.' '.$obj->y.'<br/>';
+echo 'Summmation :'.$obj->sum().'<br/>';
+echo 'Subtruciton :'.$obj->sub().'<br/>';
+echo 'Multiplication :'.$obj->mul().'<br/>';
+echo 'Division :'.$obj->div().'<br/>';
 
 ?>
